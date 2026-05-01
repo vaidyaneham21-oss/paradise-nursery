b7cca66a51460f803274375206499f40abec8044
vaidyaneham21-oss <vaidyaneham21@gmail.com>
2026-05-01 13:36:29 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,21 @@
-import React from "react";
+import { useState } from "react";
+import ProductList from "./components/ProductList";
 
 function App() {
+  const [showProducts, setShowProducts] = useState(false);
+
   return (
-    <div>
-      <h1>Paradise Nursery</h1>
-      <button>Get Started</button>
+    <div className="background-image">
+      {!showProducts ? (
+        <div>
+          <h1>Welcome to Paradise Nursery</h1>
+          <button onClick={() => setShowProducts(true)}>
+            Get Started
+          </button>
+        </div>
+      ) : (
+        <ProductList />
+      )}
     </div>
   );
 }
