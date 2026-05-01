6115ab53a92f61453199003b573ab796e589ea80
vaidyaneham21-oss <vaidyaneham21@gmail.com>
2026-05-01 13:32:31 +0530

    Refactor CartSlice actions to use Redux Toolkit

--- src/redux/CartSlice.jsx
+++ src/redux/CartSlice.jsx
@@ -2,16 +2,19 @@ const initialState = {
   items: []
 };
 
-export const addItem = (item) => {
-  initialState.items.push(item);
+export const addItem = (state, action) => {
+  state.items.push(action.payload);
 };
 
-export const removeItem = (index) => {
-  initialState.items.splice(index, 1);
+export const removeItem = (state, action) => {
+  state.items = state.items.filter((item, index) => index !== action.payload);
 };
 
-export const updateQuantity = (index, quantity) => {
-  initialState.items[index].quantity = quantity;
+export const updateQuantity = (state, action) => {
+  const { index, quantity } = action.payload;
+  if (state.items[index]) {
+    state.items[index].quantity = quantity;
+  }
 };
 
 export default initialState;
