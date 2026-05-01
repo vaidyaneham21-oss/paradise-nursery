4516480acab500b14a7f19d6c0a4128cf603edb5
vaidyaneham21-oss <vaidyaneham21@gmail.com>
2026-05-01 13:20:10 +0530

    Add AboutUs component with nursery information

---
+++ src/components/AboutUs.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function AboutUs() {
+  return (
+    <div>
+      <h1>About Paradise Nursery</h1>
+      <p>We provide beautiful indoor plants.</p>
+    </div>
+  );
+}
+
+export default AboutUs;
