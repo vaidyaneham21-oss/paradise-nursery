091ba9beafe92b59c8211ae4088c6c0d5efd31c8
vaidyaneham21-oss <vaidyaneham21@gmail.com>
2026-05-01 13:22:30 +0530

    Create App component for Paradise Nursery

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div>
+      <h1>Paradise Nursery</h1>
+      <button>Get Started</button>
+    </div>
+  );
+}
+
+export default App;
