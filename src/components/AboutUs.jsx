18480f0427f164bd174c2a22ac2605c2f0da3711
vaidyaneham21-oss <vaidyaneham21@gmail.com>
2026-05-01 13:35:26 +0530

    Update AboutUs.jsx

--- src/components/AboutUs.jsx
+++ src/components/AboutUs.jsx
@@ -1,10 +1,15 @@
-import React from "react";
-
 function AboutUs() {
   return (
-    <div>
+    <div className="about-us-container">
       <h1>About Paradise Nursery</h1>
-      <p>We provide beautiful indoor plants.</p>
+      <p>
+        Paradise Nursery is a plant shop dedicated to providing beautiful indoor plants
+        that bring freshness and positivity to your home.
+      </p>
+      <p>
+        Our mission is to make greenery accessible to everyone by offering a wide variety
+        of plants at affordable prices along with expert guidance.
+      </p>
     </div>
   );
 }
